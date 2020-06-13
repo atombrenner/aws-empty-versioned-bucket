08f292b4b4900da11535ec4bcf34e403615fa534
Christian Rodemeyer <atombrenner@gmail.com>
2020-06-13 16:17:43 +0200

    fix typo in comment

--- empty-bucket.ts
+++ empty-bucket.ts
@@ -2,7 +2,7 @@ import { S3, ListObjectVersionsOutput, ObjectIdentifier } from '@aws-sdk/client-
 import { fromIni } from '@aws-sdk/credential-provider-ini'
 import { roleAssumer } from './role-assumer'
 
-// npx ts-node -T empt-bucket
+// npx ts-node -T empty-bucket
 
 const Bucket = 'some-atombrenner-bucket'
 
